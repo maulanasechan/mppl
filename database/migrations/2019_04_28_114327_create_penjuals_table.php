577b0a4e91fa00aaa5ff856447bd59f3a8d1d9d0
aifajar <ahmadibnu-fajar@hotmail.com>
2019-04-28 18:53:08 +0700

    database

---
+++ database/migrations/2019_04_28_114327_create_penjuals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePenjualsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjuals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password');
+            $table->string('nama_usaha');
+            $table->string('alamat'); 
+            $table->string('foto_usaha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjuals');
+    }
+}
